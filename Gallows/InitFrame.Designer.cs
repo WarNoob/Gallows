2cae4c59758636e637b9c7dce66b7336338e1890
Aleksey Linnik <dom_istorika@mail.ru>
2017-10-06 13:29:59 +0700

    Ошибка в слове "старт" была

--- Gallows/InitFrame.Designer.cs
+++ Gallows/InitFrame.Designer.cs
@@ -45,7 +45,7 @@
             this.buttonStart.Name = "buttonStart";
             this.buttonStart.Size = new System.Drawing.Size(102, 38);
             this.buttonStart.TabIndex = 0;
-            this.buttonStart.Text = "СТРАТ!";
+            this.buttonStart.Text = "СТАРТ!";
             this.buttonStart.UseVisualStyleBackColor = true;
             // 
             // label1
