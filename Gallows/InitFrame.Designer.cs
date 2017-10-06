9864f5e60246fd9ed937bc5182c6f9087702b33d
Aleksey Linnik <dom_istorika@mail.ru>
2017-10-06 13:29:16 +0700

    Переименовал texBox ввода имени игрока. Шабы было понятно чё за texBox

--- Gallows/InitFrame.Designer.cs
+++ Gallows/InitFrame.Designer.cs
@@ -31,7 +31,7 @@
             this.buttonStart = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxPlayerName = new System.Windows.Forms.TextBox();
             this.radioButtonEasile = new System.Windows.Forms.RadioButton();
             this.radioButtonFine = new System.Windows.Forms.RadioButton();
             this.radioButtonHard = new System.Windows.Forms.RadioButton();
@@ -66,12 +66,12 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Сложность";
             // 
-            // textBox1
+            // textBoxPlayerName
             // 
-            this.textBox1.Location = new System.Drawing.Point(89, 21);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 3;
+            this.textBoxPlayerName.Location = new System.Drawing.Point(89, 21);
+            this.textBoxPlayerName.Name = "textBoxPlayerName";
+            this.textBoxPlayerName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPlayerName.TabIndex = 3;
             // 
             // radioButtonEasile
             // 
@@ -113,7 +113,7 @@
             this.Controls.Add(this.radioButtonHard);
             this.Controls.Add(this.radioButtonFine);
             this.Controls.Add(this.radioButtonEasile);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxPlayerName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonStart);
@@ -131,7 +131,7 @@
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxPlayerName;
         private System.Windows.Forms.RadioButton radioButtonEasile;
         private System.Windows.Forms.RadioButton radioButtonFine;
         private System.Windows.Forms.RadioButton radioButtonHard;
