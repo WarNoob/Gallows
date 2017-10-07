6359943766f734d149162de7a0174b9d86566583
Бухтияров Евгений Викторович <Bukhtiyarov.Evgeniy@euroset.lan>
2017-10-07 13:40:01 +0700

    Commit from Еугенминер

--- Gallows/GameFrame.Designer.cs
+++ Gallows/GameFrame.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(330, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 86);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Нажми меня";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // GameFrame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(860, 528);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "GameFrame";
@@ -44,6 +67,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Gallows/GameFrame.cs
+++ Gallows/GameFrame.cs
@@ -16,5 +16,10 @@ namespace Gallows
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2_Click.Text("Не нажимай меня!!!")
+        }
     }
 }
