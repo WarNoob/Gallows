ed8d51c0ae2741f236bb125691494e43bd2dfdcd
Бухтияров Евгений Викторович <Bukhtiyarov.Evgeniy@euroset.lan>
2017-10-10 18:18:05 +0700

    чуть улучшил и камешки разные сделал на пробу

--- Gallows/GameFrame.cs
+++ Gallows/GameFrame.cs
@@ -12,20 +12,23 @@ namespace Gallows
 {
     public partial class GameFrame : Form
     {
+
+        private float scale = 0.1F;
+        private int scale_c = 0;
+        private bool bboool = false;
+
         public GameFrame()
         {
             InitializeComponent();
         }
 
+        // При загрузке основного окна игры
         private void GameFrame_Load(object sender, EventArgs e)
         {
-            pictureBox1.MouseEnter += new EventHandler(pictureBox1_MouseEnter);
-            pictureBox1.MouseLeave += new EventHandler(pictureBox1_MouseLeave);
+
         }
 
-        private float scale = 0.1F;
-        private int scale_c = 0;
-        private bool bboool = false;
+        // Обработчик таймера плавнопоявляющий надпись старт
         private void timer1_Tick_1(object sender, EventArgs e)
         {
             pictureBox1.Size = new Size((int)(765 * scale), (int)(460 * scale));
@@ -39,8 +42,11 @@ namespace Gallows
             if (scale >= 1)
             {
                 start.Enabled = false;
+                pictureBox1.MouseEnter += new EventHandler(pictureBox1_MouseEnter);
+                pictureBox1.MouseLeave += new EventHandler(pictureBox1_MouseLeave);
             };
         }
+        // Обработчик таймера плавнопоявляющий надпись gameover или youwin
         private void timer2_Tick(object sender, EventArgs e)
         {
             pictureBox1.Size = new Size((int)(560 * scale), (int)(130 * scale));
@@ -56,19 +62,26 @@ namespace Gallows
                 end.Enabled = false;
             };
         }
-
-
-
+        
+        // при наведении(покидании) Мыши пикчербокс становиться чуть меньше(больше)
         private void pictureBox1_MouseEnter(object sender, EventArgs e)
         {
             if (!bboool)
-                pictureBox1.Image = Image.FromFile("pics/start2.png");
+            {
+                pictureBox1.Size = new Size((int)(765 * .9), (int)(460 * .9));
+                pictureBox1.Location = new Point(383 - (pictureBox1.Size.Width / 2), 230 - (pictureBox1.Size.Height / 2));
+            }
         }
         private void pictureBox1_MouseLeave(object sender, EventArgs e)
         {
-            if(!bboool)
-                pictureBox1.Image = Image.FromFile("pics/start1.png");
+            if (!bboool)
+            {
+                pictureBox1.Size = new Size(765, 460);
+                pictureBox1.Location = new Point(383 - (pictureBox1.Size.Width / 2), 230 - (pictureBox1.Size.Height / 2));
+            }
         }
+
+        // При нажатии на большой Старт
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             if (!bboool)
@@ -80,7 +93,6 @@ namespace Gallows
             };
         }
         
-
         private void button1_Click(object sender, EventArgs e)
         {
             pictureBox2.Image = Image.FromFile("pics/visel0.png");
@@ -158,6 +170,11 @@ namespace Gallows
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
